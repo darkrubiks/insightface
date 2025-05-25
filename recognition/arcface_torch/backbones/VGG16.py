af1ec38900ab1ad83346626680a2302d4eed6611
darkrubiks <leonardoantunes123@gmail.com>
2025-05-25 20:39:47 -0300

    removed avgpool layer

--- recognition/arcface_torch/backbones/VGG16.py
+++ recognition/arcface_torch/backbones/VGG16.py
@@ -1,5 +1,5 @@
 import torch
-from torchvision.models import vgg16_bn
+from torchvision.models import vgg16_bn, vgg16
 import torch.nn as nn
 
 class VGG16(nn.Module):
@@ -7,7 +7,9 @@ class VGG16(nn.Module):
         super(VGG16, self).__init__()
         self.vgg = vgg16_bn(dropout=dropout)
         self.vgg.classifier = nn.Identity()
+        self.vgg.avgpool = nn.Identity()
         self.embeddings = nn.Sequential(
+            nn.Flatten(),
             nn.Linear(512 * 7 * 7, num_classes, bias=False),
             nn.BatchNorm1d(num_classes)
         )
@@ -17,3 +19,11 @@ class VGG16(nn.Module):
         logits = self.embeddings(feats)
 
         return logits
+    
+if __name__ == "__main__":
+    model = VGG16(num_classes=512)
+    print(model)
+    model.eval()
+    x = torch.randn(1, 3, 224, 224)  # Example input
+    output = model(x)
+    print(output.shape)  # Should be [1, 512]
